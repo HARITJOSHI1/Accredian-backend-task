61941cdd0670c8d6bd0007598e1131f297053010
HARITJOSHI1 <joshiharit3@gmail.com>
2024-07-06 15:10:58 +0530

    minor change

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
+
+app.get("/", (_, res) => res.end("Working...."));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
